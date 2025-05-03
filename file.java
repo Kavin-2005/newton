4fb1bd6c8b100aecb8810dc4fbf341ce9e40855b
Kavin.S <senthilkavi1972@gmail.com>
2025-05-03 18:58:10 +0530

    Create file.java

---
+++ file.java
@@ -0,0 +1,20 @@
+// BankAccount.java
+public class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+    // Constructor
+    public BankAccount(String name, double initialBalance) {
+        accountHolder = name;
+        balance = initialBalance;
+    }
+
+    // Deposit money
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
